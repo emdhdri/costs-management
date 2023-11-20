31c9429ea68523ea99f30e8bf8506b520e6086a2
Emad <emdhdri@gmail.com>
2023-11-20 17:40:16 +0330

    update models and debug views

--- app/models.py
+++ app/models.py
@@ -5,16 +5,16 @@ me.connect('t')
 class User(me.Document):
     first_name = me.StringField()
     last_name = me.StringField()
-    username = me.StringField(required=True)
+    username = me.StringField(required=True, unique=True)
     #password will be added and it is omitted now
     birth_date = me.DateField()
 
     def to_dict(self):
-        user_birth_date = self.birth_date.to_python().isoformat() if self.birth_date else None
+        user_birth_date = self.birth_date.isoformat() if self.birth_date else None
         data = {
-            'first_name' : self.first_name.to_python(),
-            'last_name' : self.last_name.to_python(),
-            'username' : self.username.to_python(),
+            'first_name' : self.first_name,
+            'last_name' : self.last_name,
+            'username' : self.username,
             'birth_date' : user_birth_date,
         }
         return data
@@ -26,7 +26,7 @@ class User(me.Document):
             user_birth_date = datetime.fromisoformat(data['birth_date'])
             setattr(self, 'birth_date', user_birth_date)
         
-        for field in ['firs_name', 'last_name', 'username']:
+        for field in ['first_name', 'last_name', 'username']:
             if(field in data):
                 setattr(self, field, data[field])
 
@@ -36,13 +36,14 @@ class Expense(me.Document):
     date = me.DateTimeField()
     user_ref = me.ReferenceField(User, required=True)
     
-    def to_dict(self):
-        expense_date = self.date.to_python().isoformat() if self.date else None
+    def to_dict(self, include_user=True):
+        expense_date = self.date.isoformat() if self.date else None
         data = {
-            'cost' : self.cost.to_python(),
+            'cost' : self.cost,
             'date' : expense_date,
-            'user' : self.user_ref.to_dict(),
         }
+        if(include_user and self.user_ref):
+            data['user'] = self.user_ref.to_dict()
         return data
     
     def from_dict(self, data):
@@ -50,6 +51,9 @@ class Expense(me.Document):
             return
         if('user_ref' not in data):
             return
-        for field in ['cost', 'user_ref', 'date']:
+        if('date' in data):
+            expense_date = datetime.fromisoformat(data['date'])
+            setattr(self, 'date', expense_date)
+        for field in ['cost', 'user_ref']:
             if(field in data):
                 setattr(self, field, data[field])
--- app/routes.py
+++ app/routes.py
@@ -1,53 +1,72 @@
 from app import app
 from app.models import User, Expense
-from flask import jsonify, request
+from flask import jsonify, request, abort, Response
 import mongoengine as me
 
 @app.route('/')
 def index():
     pass
 
+@app.errorhandler(404)
+def resource_not_found(e):
+    return jsonify(status = 404, error=str(e))
+
+@app.errorhandler(409)
+def duplicate_resource(e):
+    return jsonify(status = 409, error=str(e))
+
+
 @app.route('/users', methods=['GET'])
 def get_users():
     data = [user.to_dict() for user in User.objects()]
     return jsonify(data)
 
+
 @app.route('/users/<string:username>', methods=['GET'])
 def get_user(username):
     try:
-        user = User.objects.get(usernmae=username)
+        user = User.objects.get(username=username).to_dict()
     except User.DoesNotExist:
-        return "Not found 404"
+        abort(404, description="Resource not found")
     
     return jsonify(user)
+
 
 @app.route('/users/<string:username>/expenses', methods=['GET'])
 def get_expenses(username):
     try:
-        user = User.objects.get(usernmae=username)
+        user = User.objects.get(username=username)
     except User.DoesNotExist:
-        return "Not found 404"
+        abort(404, description='Resource not found')
     
-    user_expenses = [expense.to_dict() for expense in Expense.objects(user_ref=user)]
+    user_expenses = [expense.to_dict() for expense in Expense.objects(user_ref=user).exclude('user_ref')]
     return jsonify(user_expenses)
+
 
 @app.route('/users', methods=['POST'])
 def create_user():
     data = request.get_json() or {}
     user = User()
     user.from_dict(data)
-    user.save()
+    try:
+        user.save()
+    except me.errors.NotUniqueError:
+        abort(409, description='duplicate resource')
+    return jsonify(status=200)
+
 
 @app.route('/users/<string:username>/expenses', methods=['POST'])
 def create_expense(username):
     try:
-        user = User.objects.get(usernmae=username)
+        user = User.objects.get(username=username)
     except User.DoesNotExist:
-        return "Not found 404"
+        abort(404, description="Resource not found")
     
     data = request.get_json() or {}
     data['user_ref'] = user
     expense = Expense()
     expense.from_dict(data)
     expense.save()
-    return "OK"
+    return jsonify(status=200)
+
+#@app.route('/users/<string:username>/expenses/<')
