9ee0363dbd5a888b540ae64d8c788a3de3c0722e
Emad <emdhdri@gmail.com>
2023-11-20 16:12:46 +0330

    update models and create views

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 import mongoengine as me
 from datetime import datetime
 
+me.connect('t')
 class User(me.Document):
     first_name = me.StringField()
     last_name = me.StringField()
@@ -8,8 +9,47 @@ class User(me.Document):
     #password will be added and it is omitted now
     birth_date = me.DateField()
 
+    def to_dict(self):
+        user_birth_date = self.birth_date.to_python().isoformat() if self.birth_date else None
+        data = {
+            'first_name' : self.first_name.to_python(),
+            'last_name' : self.last_name.to_python(),
+            'username' : self.username.to_python(),
+            'birth_date' : user_birth_date,
+        }
+        return data
+    
+    def from_dict(self, data):
+        if('username' not in data):
+            return
+        if('birth_date' in data):
+            user_birth_date = datetime.fromisoformat(data['birth_date'])
+            setattr(self, 'birth_date', user_birth_date)
+        
+        for field in ['firs_name', 'last_name', 'username']:
+            if(field in data):
+                setattr(self, field, data[field])
+
+
 class Expense(me.Document):
     cost = me.DecimalField(required=True)
     date = me.DateTimeField()
     user_ref = me.ReferenceField(User, required=True)
     
+    def to_dict(self):
+        expense_date = self.date.to_python().isoformat() if self.date else None
+        data = {
+            'cost' : self.cost.to_python(),
+            'date' : expense_date,
+            'user' : self.user_ref.to_dict(),
+        }
+        return data
+    
+    def from_dict(self, data):
+        if('cost' not in data):
+            return
+        if('user_ref' not in data):
+            return
+        for field in ['cost', 'user_ref', 'date']:
+            if(field in data):
+                setattr(self, field, data[field])
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,53 @@
 from app import app
 from app.models import User, Expense
 from flask import jsonify, request
+import mongoengine as me
 
 @app.route('/')
 def index():
     pass
 
+@app.route('/users', methods=['GET'])
+def get_users():
+    data = [user.to_dict() for user in User.objects()]
+    return jsonify(data)
+
+@app.route('/users/<string:username>', methods=['GET'])
+def get_user(username):
+    try:
+        user = User.objects.get(usernmae=username)
+    except User.DoesNotExist:
+        return "Not found 404"
+    
+    return jsonify(user)
+
+@app.route('/users/<string:username>/expenses', methods=['GET'])
+def get_expenses(username):
+    try:
+        user = User.objects.get(usernmae=username)
+    except User.DoesNotExist:
+        return "Not found 404"
+    
+    user_expenses = [expense.to_dict() for expense in Expense.objects(user_ref=user)]
+    return jsonify(user_expenses)
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    data = request.get_json() or {}
+    user = User()
+    user.from_dict(data)
+    user.save()
+
+@app.route('/users/<string:username>/expenses', methods=['POST'])
+def create_expense(username):
+    try:
+        user = User.objects.get(usernmae=username)
+    except User.DoesNotExist:
+        return "Not found 404"
+    
+    data = request.get_json() or {}
+    data['user_ref'] = user
+    expense = Expense()
+    expense.from_dict(data)
+    expense.save()
+    return "OK"
